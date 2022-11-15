5dba301f3fc16c4e7550667c9b7232ab5cb47244
coderof19clc7 <mailluubaihoc@gmail.com>
2022-11-15 22:12:53 +0700

    fix conflict login ui

--- src/Components/LoginForm/index.jsx
+++ src/Components/LoginForm/index.jsx
@@ -13,57 +13,77 @@ function LoginForm() {
     const onSubmit = (data) => console.log(data);
 
     return (
-        <div className="w-full md:max-w-xl bg-white shadow-lg rounded px-8 pt-8 pb-6">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="mb-3">
-                    <label className="block text-sm font-medium text-gray-700" htmlFor="username">
-                        Username
-                        <input
-                            name="username"
-                            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1
-                                        block w-full sm:text-sm border border-gray-300 rounded-md"
-                            id="username"
-                            type="text"
-                            placeholder="Quamon"
-                            {...register("username", { required: true })}
-                        />
-                        {errors.username && (
-                            <span className="text-red-600">This field is required</span>
-                        )}
-                    </label>
-                </div>
-                <div className="mb-3">
-                    <label className="block text-sm font-medium text-gray-700" htmlFor="password">
-                        Password
-                        <input
-                            type="password"
-                            name="password"
-                            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1
-                                        block w-full sm:text-sm border border-gray-300 rounded-md"
-                            id="password"
-                            placeholder="********"
-                            {...register("password", { required: true }, { minLength: 8 })}
-                        />
-                        {errors.password && (
-                            <span className="text-red-600">This field is required</span>
-                        )}
-                    </label>
-                </div>
-                <div className="py-3 bg-gray-50 text-right flex flex-row justify-between">
-                    <button
-                        type="submit"
-                        className="inline-flex justify-center py-2 px-4 border border-transparent
-                                    shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600
-                                    hover:bg-indigo-700 focus:outline-none focus:ring-2
-                                    focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Sign in
-                    </button>
-                    <div className="text-center text-gray-500">
-                        <span>New to site?</span>&nbsp;
+        <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
+            <div className="w-full md:max-w-xl bg-white shadow-lg rounded px-8 pt-8 pb-6">
+                <h1 className="text-3xl font-semibold text-center text-purple-700 uppercase">
+                    Sign in
+                </h1>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="username-input mb-3">
+                        <label
+                            className="block text-sm font-medium text-gray-700"
+                            htmlFor="username"
+                        >
+                            Username
+                            <input
+                                name="username"
+                                className="shadow-sm 
+                                focus:ring-indigo-500 focus:border-indigo-500 mt-1
+                                block w-full sm:text-sm border border-gray-300 rounded-md
+                                px-2 py-2 bg-white border rounded-md "
+                                id="username"
+                                type="text"
+                                placeholder="Quamon"
+                                {...register("username", { required: true })}
+                            />
+                            {errors.username && (
+                                <span className="text-red-600">This field is required</span>
+                            )}
+                        </label>
+                    </div>
+                    <div className="password-input mb-3">
+                        <label
+                            className="block text-sm font-medium text-gray-700"
+                            htmlFor="password"
+                        >
+                            Password
+                            <input
+                                type="password"
+                                name="password"
+                                className="shadow-sm 
+                                focus:ring-indigo-500 focus:border-indigo-500 mt-1
+                                block w-full sm:text-sm border border-gray-300 rounded-md
+                                px-2 py-2 bg-white border rounded-md "
+                                id="password"
+                                placeholder="********"
+                                {...register("password", { required: true }, { minLength: 8 })}
+                            />
+                            {errors.password && (
+                                <span className="text-red-600">This field is required</span>
+                            )}
+                        </label>
+                    </div>
+                    <div className="forgot-pwd-btn mb-2">
                         <Link
                             to="/register"
-                            className="underline hover:underline hover:text-blue-800"
+                            class=" text-sm text-gray-500 no-underline hover:underline hover:text-blue-800"
+                        >
+                            Forgot password?
+                        </Link>
+                    </div>
+                    <div className="mt-6">
+                        <button
+                            type="submit"
+                            className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
+                        >
+                            Login
+                        </button>
+                    </div>
+                    <div className="mt-3 text-gray-500 text-center">
+                        <span>Don&#39;t have account?</span>&nbsp;
+                        <Link
+                            to="/register"
+                            class="no-underline hover:no-underline hover:text-blue-800 font-bold"
                         >
                             Register
                         </Link>
@@ -75,8 +95,8 @@ function LoginForm() {
                         Sign in with
                     </div>
                     <SocialSignInBtns />
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     );
 }
