df40f01aace3eb77d6df6d27521dd00f6c99dfb9
Nguyễn Năng Tiến <tien.nnang@gmail.com>
2022-11-22 22:24:33 +0700

    Temp config for oauth02

--- src/pages/login/socialSignInBtns.jsx
+++ src/pages/login/socialSignInBtns.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { FaFacebookF, FaGoogle } from "react-icons/fa";
 
 function SocialSignInBtns() {
+    const google = () => {
+        window.open("http://localhost:5000/auth/google", "_self");
+    };
+
     return (
         <div className="flex justify-center mt-4 gap-x-2">
             <button
@@ -16,6 +20,7 @@ function SocialSignInBtns() {
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
                 className="text-white bg-[#ea4335] font-medium rounded-full text-sm p-3 text-center inline-flex items-center mr-2 mb-2"
+                onClick={google}
             >
                 <FaGoogle className="w-5 h-5" />
             </button>
