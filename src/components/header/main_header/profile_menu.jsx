c8aa0b20d16054915753d7c065d1a60b449a34b4
coderof19clc7 <63506231+coderof19clc7@users.noreply.github.com>
2022-11-24 14:36:35 +0700

    Update profile_menu.jsx

--- src/components/header/main_header/profile_menu.jsx
+++ src/components/header/main_header/profile_menu.jsx
@@ -36,7 +36,7 @@ function ProfileMenu() {
             </div>
             <div
                 hidden={!isProfileMenuOpen}
-                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                className="absolute right-0 z-50 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                 role="menu"
                 aria-orientation="vertical"
                 aria-labelledby="user-menu-button"
